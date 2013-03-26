5472fa8c7b4aa509013ccee1d88cf1b8b5088e60
mvergerdelbove <mvergerdelbove@work4labs.com>
2013-03-26 15:58:43 +0100

    test_project: the part on create/delete/create_structure is not relevent for now

--- tests/test_project.py
+++ tests/test_project.py
@@ -23,34 +23,37 @@ class TestProject(unittest.TestCase):
         for dir_name in ():
             self.connection.delete_webdav_dir(dir_name)
 
-    def test_create_and_delete_project(self):
-        if not project_id:
-            project = Project(self.connection).create(TEST_PROJECT_NAME)
-        else:
-            project = Project(self.connection).load(id=project_id)
-        self.assert_(project is not None)
-        self.assert_(project.id is not None)
-        if not project_id:
-            project.delete()
-            self.assertRaises(ProjectNotOpenedError, project.delete)
-            self.assertRaises(ProjectNotFoundError, project.load,
-                              name=TEST_PROJECT_NAME)
+# FIXME : those tests are useless since we cannot create another project on our
+#         enterprise plan on GD.
 
-    def test_create_structure(self):
-        if not project_id:
-            project = Project(self.connection).create(TEST_PROJECT_NAME)
-        else:
-            project = Project(self.connection).load(id=project_id)
+#     def test_create_and_delete_project(self):
+#         if not project_id:
+#             project = Project(self.connection).create(TEST_PROJECT_NAME)
+#         else:
+#             project = Project(self.connection).load(id=project_id)
+#         self.assert_(project is not None)
+#         self.assert_(project.id is not None)
+#         if not project_id:
+#             project.delete()
+#             self.assertRaises(ProjectNotOpenedError, project.delete)
+#             self.assertRaises(ProjectNotFoundError, project.load,
+#                               name=TEST_PROJECT_NAME)
 
-        for (example, ExampleDataset) in examples.examples:
-            dataset = ExampleDataset(project)
-            self.assertRaises(DataSetNotFoundError, dataset.get_metadata,
-                              name=dataset.schema_name)
-            dataset.create()
-            self.assert_(dataset.get_metadata(name=dataset.schema_name))
+#     def test_create_structure(self):
+#         if not project_id:
+#             project = Project(self.connection).create(TEST_PROJECT_NAME)
+#         else:
+#             project = Project(self.connection).load(id=project_id)
 
-        if not project_id:
-            project.delete()
+#         for (example, ExampleDataset) in examples.examples:
+#             dataset = ExampleDataset(project)
+#             self.assertRaises(DataSetNotFoundError, dataset.get_metadata,
+#                               name=dataset.schema_name)
+#             dataset.create()
+#             self.assert_(dataset.get_metadata(name=dataset.schema_name))
+
+#         if not project_id:
+#             project.delete()
 
     def test_validate_maql(self):
         if not project_id:
