a6547440c86725b9fbf1e5f6dbf9640f13b09225
mvergerdelbove <mvergerdelbove@work4labs.com>
2013-04-12 10:38:00 +0200

    use a custom csv file writer, to handler unicode; rewrite every file overture with context manager

--- gooddataclient/archiver.py
+++ gooddataclient/archiver.py
@@ -1,63 +1,85 @@
 import os
-import csv
-import simplejson as json
 from tempfile import mkstemp
 from zipfile import ZipFile
 import datetime
 import hashlib
 from collections import Iterable
 
+import simplejson as json
+
 
 DLI_MANIFEST_FILENAME = 'upload_info.json'
 CSV_DATA_FILENAME = 'data.csv'
 DEFAULT_ARCHIVE_NAME = 'upload.zip'
 
+
+def csv_encode(val):
+    if not isinstance(val, basestring):
+        val = str(val)
+    return '"' + val.replace('"', '""') + '"'
+
+
+def value_list_from_dict(values, fields):
+    return [values[field] for field in fields]
+
+
+def write_csv_line(file_handle, values):
+    """
+    A function to write a csv line,
+    as csv python module does not handle
+    """
+    fmt_values = map(csv_encode, values)
+    file_handle.write((','.join(fmt_values)).encode('UTF-8'))
+    file_handle.write("\n")
+
+
 def write_tmp_file(content):
     '''Write any data to a temporary file.
     Remember to os.remove(filename) after use.
-    
+
     @param content: data to be written to a file
-    
+
     return filename of the created temporary file
     '''
     fp, filename = mkstemp()
-    file = open(filename, 'w+b')
-    file.write(content)
-    os.close(fp)
+
+    with open(filename, 'w+b') as file:
+        file.write(content)
+
     return filename
+
 
 def write_tmp_csv_file(csv_data, sli_manifest):
     '''Write a CSV temporary file with values in csv_data - list of dicts.
-    
+
     @param csv_data: list of dicts
     @param sli_manifest: json sli_manifest
     '''
     fieldnames = [part['columnName'] for part in sli_manifest['dataSetSLIManifest']['parts']]
     fp, filename = mkstemp()
-    file = open(filename, 'w+b')
-    writer = csv.DictWriter(file, fieldnames=fieldnames,
-                            delimiter=sli_manifest['dataSetSLIManifest']['csvParams']['separatorChar'],
-                            quotechar=sli_manifest['dataSetSLIManifest']['csvParams']['quoteChar'],
-                            quoting=csv.QUOTE_ALL)
-    headers = dict((n, n) for n in fieldnames)
-    writer.writerow(headers)
-    for line in csv_data:
-        for key in fieldnames:
-            #some incredible magic with additional date field
-            if not key in line and key.endswith('_dt'):
-                h = hashlib.md5()
-                #h.update(line[key[:-3]])
-                h.update('123456')
-                line[key] = h.hexdigest()[:6]
-            #formatting the date properly
-            if isinstance(line[key], datetime.datetime):
-                line[key] = line[key].strftime("%Y-%m-%d")
-            #make 0/1 from bool
-            if isinstance(line[key], bool):
-                line[key] = int(line[key])
-        writer.writerow(line)
-    os.close(fp)
+
+    with open(filename, 'w+b') as file:
+
+        write_csv_line(file, fieldnames)
+
+        for line in csv_data:
+            for key in fieldnames:
+                #some incredible magic with additional date field
+                if not key in line and key.endswith('_dt'):
+                    h = hashlib.md5()
+                    h.update(line[key[:-3]])
+                    line[key] = h.hexdigest()[:6]
+                #formatting the date properly
+                if isinstance(line[key], datetime.datetime):
+                    line[key] = line[key].strftime("%Y-%m-%d")
+                #make 0/1 from bool
+                if isinstance(line[key], bool):
+                    line[key] = int(line[key])
+
+            write_csv_line(file, value_list_from_dict(line, fieldnames))
+
     return filename
+
 
 def write_tmp_zipfile(files):
     '''Zip files into a single file.
@@ -68,12 +90,13 @@ def write_tmp_zipfile(files):
     return filename of the created temporary zip file
     '''
     fp, filename = mkstemp()
-    zip_file = ZipFile(filename, "w")
-    for path, name in files:
-        zip_file.write(path, name)
-    zip_file.close()
-    os.close(fp)
+
+    with ZipFile(filename, "w") as zip_file:
+        for path, name in files:
+            zip_file.write(path, name)
+
     return filename
+
 
 def create_archive(data, sli_manifest):
     """
