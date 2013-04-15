d4cff426a3b0d0e495f87eba06ff77fc9f5ac245
mvergerdelbove <mvergerdelbove@work4labs.com>
2013-04-15 10:04:27 +0200

    add docstring to upload method of dataset, and only create/check metadata if no_upload=False

--- gooddataclient/dataset.py
+++ gooddataclient/dataset.py
@@ -109,16 +109,27 @@ class Dataset(object):
 
     def upload(self, keep_csv=False, csv_file=None,
                no_upload=False, *args, **kwargs):
-        try:
-            self.get_metadata(self.schema_name)
-        except DataSetNotFoundError:
-            self.create()
+        """
+        A function to upload dataset data. It tries to
+        call the data() method of the dataset to retrive
+        data. If `no_upload` is set to True, no dataset is
+        created nor uploaded on the project.
+
+        If `keep_csv` is set to True, a csv dump is kept, in
+        the file given by `csv_file`.
+        """
+        if not no_upload:
+            try:
+                self.get_metadata(self.schema_name)
+            except DataSetNotFoundError:
+                self.create()
 
         dates, datetimes = self.get_datetime_column_names()
         dir_name = self.connection.webdav.upload(
             self.data(*args, **kwargs), self.get_sli_manifest(),
             dates, datetimes, keep_csv, csv_file, no_upload
         )
+
         if not no_upload:
             self.project.integrate_uploaded_data(dir_name)
             self.connection.webdav.delete(dir_name)
