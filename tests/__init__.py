ddeee8ecdf8793e9bd6386befa65cea9fdea27f5
mvergerdelbove <mvergerdelbove@work4labs.com>
2013-05-13 12:29:48 +0200

    import all the tests in tests.__init__

---
+++ tests/__init__.py
@@ -0,0 +1,7 @@
+from test_text import *
+from test_schema import *
+from test_archiver import *
+from test_connection import *
+from test_project import *
+from test_dataset import *
+from test_migration import *
